eed76f26bbb4ba42edbc980269b9a13fb158e1a3
Tyler Camp <tylerc5@illinois.edu>
2017-09-15 16:49:20 -0500

    better logging

---
+++ Sim.py
@@ -0,0 +1,186 @@
+# Simulating ribosomes, probably
+# by TC
+
+import hoomd
+import hoomd.md
+import random
+import math
+import string
+import numpy as np
+import time  # is there a real life analog for this import statement?
+import datetime
+import re
+import sys
+import tmeanalysis
+
+# All length units specified in nanometers
+
+# Simulation params
+T = 300 # Temperature in Kelvin
+k_b = 1.380648e-23 # Boltzmann constant, in J/K
+kT = k_b * T
+seed = 42
+dt = 1e-9
+use_walls = True
+frame_period = 1 # how often to write trajectories
+
+# For a spherical cell with diameter 400nm, use a lattice 
+# with side length 322.39839nm (~322) to approximate the same cell volume.
+box_size = 322e-9 # length of any one side of simulation box
+
+# Ribosomes
+m_rib = 2700000
+diff_rib =  5e-14 # ribosome diffusion constant
+diam_rib = 20e-9
+gamma_r = kT / diff_rib # Friction coefficient 
+
+# Protein
+m_prot = 346000
+diff_prot = 10e-12 # protein diffusion constant
+diam_prot = 2e-9
+gamma_p = kT / diff_prot
+
+def run():
+
+    # Initialize the context
+    hoomd.context.initialize("")
+
+    # Read in a particle configuration
+    particles = list()
+
+
+    with open('particle_config.txt', 'r') as infile:
+        for line in infile:
+            items = string.split(line, ',') 
+            species, vdwr, x, y, z = items 
+            pos = [float(x), float(y), float(z)]
+            particles.append([species, vdwr, pos])
+
+
+    # Create a snapshot
+    num_particles = len(particles)
+
+    snapshot = hoomd.data.make_snapshot(N=num_particles, 
+                    box=hoomd.data.boxdim(L=box_size, dimensions=3), 
+                    particle_types=['R','P'])
+
+    for i in range(num_particles):
+        if (particles[i][0] == 'R'):
+            snapshot.particles.typeid[i] = 0
+            snapshot.particles.diameter[i] = diam_rib
+        else:
+            snapshot.particles.typeid[i] = 1
+            snapshot.particles.diameter[i] = diam_prot
+
+        snapshot.particles.position[i] = particles[i][2] 
+
+    system = hoomd.init.read_snapshot(snapshot)
+    system_particles = system.particles
+
+    # Make a list of cells
+    nl = hoomd.md.nlist.cell(r_buff=1e-9)
+
+    # Lennard-Jones potential parameters
+    rr_sigma = diam_rib/2 
+    #rr_epsilon = 1.69e-20
+    rr_epsilon = 0
+    rp_sigma = (diam_rib/2 + diam_prot/2)/2
+    #rp_epsilon = 1.69e-20 
+    rp_epsilon = 0 
+    pp_sigma = diam_prot/2
+    #pp_epsilon = 1.69e-20 
+    pp_epsilon = 0 
+    rr_cutoff = diam_rib*5
+    rp_cutoff = diam_rib*5
+    pp_cutoff = diam_prot*5
+    lj = hoomd.md.pair.lj(r_cut=rr_cutoff, nlist=nl)
+    alpha = 1.0
+    lj.pair_coeff.set('R','R', epsilon=rr_epsilon, sigma=rr_sigma, alpha=alpha)
+    lj.pair_coeff.set('R','P', epsilon=rp_epsilon, sigma=rp_sigma, r_cut=rp_cutoff, alpha=alpha)
+    lj.pair_coeff.set('P','P', epsilon=pp_epsilon, sigma=pp_sigma, r_cut=pp_cutoff, alpha=alpha)
+
+
+    # Wall potentials
+    if use_walls:
+        wall_bottom = hoomd.md.wall.plane(origin=(0, 0, -box_size/2), normal=(0, 0, 1.0), inside=True)
+        wall_top = hoomd.md.wall.plane(origin=(0, 0, box_size/2), normal=(0, 0, -1.0), inside=True)
+        wall_negX = hoomd.md.wall.plane(origin=(-box_size/2, 0, 0), normal=(1.0, 0, 0), inside=True)
+        wall_posX = hoomd.md.wall.plane(origin=(box_size/2, 0, 0), normal=(-1.0, 0, 0), inside=True)
+        wall_negY = hoomd.md.wall.plane(origin=(0, -box_size/2, 0), normal=(0, 1.0, 0), inside=True)
+        wall_posY = hoomd.md.wall.plane(origin=(0, box_size/2, 0), normal=(0, -1.0, 0), inside=True)
+        wall_lst = [wall_bottom, wall_top, wall_negX, wall_negY, wall_posX, wall_posY]
+        all_walls = hoomd.md.wall.group(wall_lst)
+        wlj = hoomd.md.wall.lj(all_walls, r_cut=diam_rib*5)
+        wlj.force_coeff.set('R', sigma=rr_sigma, epsilon=rr_epsilon, alpha=alpha)
+        wlj.force_coeff.set('P', sigma=rr_sigma, epsilon=rr_epsilon, alpha=alpha)
+        wlj.force_coeff.set(['R','P'], sigma=rp_sigma, epsilon=rp_epsilon, alpha=alpha)
+
+
+    # Set up the simulation
+    hoomd.md.integrate.mode_standard(dt=dt)
+    all_parts = hoomd.group.all()
+    ribosomes = hoomd.group.type(name='ribosomes', type='R')
+    proteins = hoomd.group.type(name='proteins', type='P')
+    print len(ribosomes)
+    print len(proteins)
+    bd = hoomd.md.integrate.brownian(group=all_parts, kT=kT, seed=seed) 
+    bd.set_gamma('R',gamma_r)
+    bd.set_gamma('P',gamma_p)
+
+    h = abs(hash((datetime.datetime.now())) + hash(seed))
+    rtraj_fname = 'rtraj' + str(h) + '.gsd'
+    ptraj_fname = 'ptraj' + str(h) + '.gsd'
+    if ribosomes:
+        hoomd.dump.gsd(rtraj_fname, period=frame_period, group=ribosomes, overwrite=True)
+    if proteins:
+        hoomd.dump.gsd(ptraj_fname, period=frame_period, group=proteins, overwrite=True)
+
+
+    # Run the simulation
+    sim_t = 1e3
+    t0 = time.clock()
+    hoomd.run(sim_t)
+    tf = time.clock()
+    elapsed = tf - t0
+
+    # Log 
+
+    fname = 'bdsim'+str(h)
+    with open(fname, 'w') as logfile:
+        logfile.write('SIMULATION PARAMETERS\n\n')
+        logfile.write('Simulation id: ' + str(h) + '\n') 
+        logfile.write('Simulation datetime: ' + str(datetime.datetime.now()) + '\n') 
+        logfile.write('Simulation volume: ' + str((box_size)**3) + '\n')
+        logfile.write('Number of particles: ' + str(num_particles) + '\n')
+        logfile.write('Temperature: ' + str(T) + '\n')
+        logfile.write('Seed: ' + str(seed) + '\n')
+        logfile.write('dt: ' + str(dt) + '\n')
+        logfile.write('Timesteps taken: ' + str(sim_t) + '\n')
+        logfile.write('Total simulation time: ' + str(sim_t * dt) + '\n')
+        logfile.write('Total wall clock time: ' + str(tf-t0) + '\n')
+        logfile.write('Ribosome diffusion coefficient: ' + str(diff_rib) + '\n')
+        logfile.write('Ribosome diameter: ' + str(diam_rib) + '\n')
+        logfile.write('Protein diffusion coefficient: ' + str(diff_prot) + '\n')
+        logfile.write('Protein diameter: ' + str(diam_prot) + '\n')
+        logfile.write('LJ epsilon for RR interaction: ' + str(rr_epsilon) + '\n')
+        logfile.write('LJ sigma for RR interaction: ' + str(rr_sigma) + '\n')
+        logfile.write('LJ epsilon for RP interaction: ' + str(rp_epsilon) + '\n')
+        logfile.write('LJ sigma for RP interaction: ' + str(rp_sigma) + '\n')
+        logfile.write('LJ epsilon for PP interaction: ' + str(pp_epsilon) + '\n')
+        logfile.write('LJ sigma for PP interaction: ' + str(pp_sigma) + '\n')
+        logfile.write('LJ alpha: ' + str(alpha) + '\n')
+        if use_walls:
+            logfile.write('Using LJ wall potential. Coefficients based on particle LJ parameters.\n')
+        logfile.write('Writing trajectories every ' + str(frame_period) + ' steps.\n') 
+        logfile.write('\n')
+    return h
+
+def main():
+
+    # Run the simulation
+    h = run()
+    
+    # Analyze the simulation
+    tmeanalysis.analyze(h, dt, diff_rib, diff_prot, box_size)    
+
+main()
---
+++ tmeanalysis.py
@@ -0,0 +1,73 @@
+import numpy as np
+# GSD file reader: git clone https://bitbucket.org/glotzer/gsd.git
+from gsd.hoomd import HOOMDTrajectory
+from gsd.fl import GSDFile
+import sys
+
+def analyze(h, dt, diff_rib, diff_prot, box_size):
+
+    fname = 'bdsim' + str(h)
+    rfname = 'rtraj' + str(h) + '.gsd'
+    pfname = 'ptraj' + str(h) + '.gsd'
+    logfile = None 
+    try:
+        logfile = open(fname, 'a')
+        logfile.write('Calculations based on trajectories:\n')
+    except EnvironmentError:
+        print "404 file not found :("
+        sys.exit(1)
+
+    ts = []
+    rib_msds = []
+    try:
+        rib_traj = HOOMDTrajectory(GSDFile(rfname, "rb", "HOOMD", "hoomd"))
+
+        rib_pos0 = rib_traj.read_frame(0).particles.position
+        frmDataRib = np.zeros((rib_traj.file.nframes, rib_pos0.shape[0], rib_pos0.shape[1]), dtype=rib_pos0.dtype)
+        ts = np.zeros(rib_traj.file.nframes)
+        for i in range(rib_traj.file.nframes):
+            frm = rib_traj.read_frame(i)
+            frmDataRib[i,...] = frm.particles.position
+            ts[i] = dt*frm.configuration.step
+        rib_max_dxs = np.max(np.abs(np.diff(frmDataRib,axis=0)), axis=(0,2))
+        frmDataFiltRib = frmDataRib[:,rib_max_dxs < 0.1*box_size,:]
+        rib_dx = frmDataFiltRib-frmDataFiltRib[0,:,:]
+        rib_msd = np.mean(np.sum(rib_dx*rib_dx, axis=2), axis=1)
+        rib_m,rib_b = np.polyfit(ts, rib_msd, 1)
+
+        mod_ts = ts[1:]
+        mod_msd = rib_msd[1:]
+        rib_diffusion = np.divide(mod_msd, (6*mod_ts))
+        rib_diffusion = np.mean(rib_diffusion[1:])
+        logfile.write("ribosome msd linear fit: m={}, b={}, slope_err={}\n".format(rib_m,rib_b, rib_m/(6*diff_rib)))
+        logfile.write("ribosome diffusion coefficient={} (error: {})\n".format(rib_diffusion, rib_diffusion/diff_rib))
+    except EnvironmentError:
+        logfile.write("No ribosome trajectory.\n")
+
+    try:
+        prot_traj = HOOMDTrajectory(GSDFile(pfname, "rb", "HOOMD", "hoomd"))
+        prot_pos0 = prot_traj.read_frame(0).particles.position
+        prot_msds = []
+        frmDataProt = np.zeros((prot_traj.file.nframes, prot_pos0.shape[0], prot_pos0.shape[1]), dtype=prot_pos0.dtype)
+        prot_ts = np.zeros(prot_traj.file.nframes)
+        for i in range(prot_traj.file.nframes):
+            frm = prot_traj.read_frame(i)
+            frmDataProt[i,...] = frm.particles.position
+        prot_max_dxs = np.max(np.abs(np.diff(frmDataProt,axis=0)), axis=(0,2))
+        frmDataFiltProt = frmDataProt[:,prot_max_dxs < 0.1*box_size,:]
+        prot_dx = frmDataFiltProt-frmDataFiltProt[0,:,:]
+        prot_msd = np.mean(np.sum(prot_dx*prot_dx, axis=2), axis=1)
+        prot_m,prot_b = np.polyfit(ts, prot_msd, 1)
+
+        mod_ts = ts[1:]
+        mod_msd = prot_msd[1:]
+        prot_diffusion = np.divide(mod_msd, (6*mod_ts))
+        prot_diffusion = np.mean(prot_diffusion[1:]) 
+        logfile.write("protein linear fit: m={}, b={}, slope_err={}\n".format(prot_m,prot_b, prot_m/(6*diff_prot)))
+        logfile.write("protein diffusion coefficient={} (error: {})\n".format(prot_diffusion, prot_diffusion/diff_prot))
+        
+    except EnvironmentError:
+        logfile.write("No protein trajectory.\n")
+
+
+    logfile.close()
